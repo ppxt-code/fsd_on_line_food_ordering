21d65716ea5fc486e965c8c4b073cb142d92b522
ppxt-code <xuantrang.phamphu@gmail.com>
2026-07-02 15:16:50 +0200

    adding icons to menu tags

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ async function loadPage(page) {
     //console.log("status:", response.status);
     //console.log("url:", response.url);
     const html = await response.text();
-    //console.log(page+" contains categories ? "+html.includes("categories"));
     app.innerHTML = html;
     if (pageInits[page]) {
         await pageInits[page]();
@@ -40,8 +39,9 @@ async function loadPage(page) {
 
 //------------------- menu :
 async function initMenuPage() {
+    addSvgToTags(); // pb when adding them to html
     try {
-        if (!hasbeenloaded) { menuItems = await getMenuItems(); hasbeenloaded = !!menuItems; } // global
+        if (!hasbeenloaded) { menuItems = await getMenuItems(); hasbeenloaded = !!menuItems; } 
         updatecartCount();
         if (menuItems) {
             let categoriesElt = document.getElementById("categories");
@@ -51,13 +51,27 @@ async function initMenuPage() {
             for (item of firstItems)
                 html += `<button onclick="categoryClicked('${item.category}')">
                             <div class="flex items-center gap-4"> 
-                                <img src="${item.image}" class="lg:w-20 lg:h-20 w-10 h-10 rounded-full transition-transform duration-300 hover:scale-110"/> 
+                                <img src="${item.image}" alt="${item.name}" class="lg:w-20 lg:h-20 w-10 h-10 rounded-full transition-transform duration-300 hover:scale-110"/> 
                                 <p class="text-orange-800 text-xl">${item.category}</p>
                             </div>
                         </button>`;
             if (categoriesElt) categoriesElt.innerHTML = html;
         }
     } catch (error) { alert(error); }
+}
+function addSvgToTags() {   
+    document.getElementById("search-tag").innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>`+document.getElementById("search-tag").innerHTML;
+    document.getElementById("offers-tag").innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 0 0-3 3v4.318a3 3 0 0 0 .879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 0 0 5.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 0 0-2.122-.879H5.25ZM6.375 7.5a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" clip-rule="evenodd" />
+</svg>`+document.getElementById("offers-tag").innerHTML;
+    document.getElementById("cart-tag").innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+</svg>`+document.getElementById("cart-tag").innerHTML;
+    document.getElementById("address-tag").innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+</svg>`+document.getElementById("address-tag").innerHTML;
 }
 function mean(array) {
     if (array.length === 0) return 0;
@@ -94,7 +108,7 @@ function fillEltWithArray(itemsArr, itemsId, appendMode=false) {
                                 </button>
                             </div>
                             <button onclick="addToCart(${item.id})">
-                                <img src="${item.image}" class="mx-auto w-24 h-24 sm:w-32 sm:h-32 rounded-full transition-transform duration-300 hover:scale-110"/> 
+                                <img src="${item.image}" alt="${item.name}" class="mx-auto w-24 h-24 sm:w-32 sm:h-32 rounded-full transition-transform duration-300 hover:scale-110"/> 
                             </button>
                             <p class="text-md font-bold text-gray-500">${item.name}</p> 
                         </div>
@@ -136,7 +150,7 @@ async function initCartPage() {
         const item = menuItems.find((item) => item.id == id); // item.id is a number, id is a string
         if (item) {
             html += `<div class="grid grid-cols-5 gap-4 font-bold items-center">
-                        <img src="${item.image}"  class="lg:w-20 lg:h-20 w-10 h-10">
+                        <img src="${item.image}" alt="${item.name}" class="lg:w-20 lg:h-20 w-10 h-10">
                         <div class="col-span-2">${item.name} Price: $${item.price}</div>
                         <div class="col-span-2">Quantity:<input type="number" min="0" value="${ids.length}" onchange="changeQuantity(${id}, Number(this.value))" class="w-20 p-2"</input></div>
                       </div>`;
@@ -158,7 +172,10 @@ function changeQuantity(id, value) {
     for (let i = 1; i <= value; i++) cart.push(id);
     initCartPage();
 }
-function updatecartCount() { document.getElementById("cart-count").textContent = `${cart.length} Items`; }
+function updatecartCount() { 
+    let cartCountElt = document.getElementById("cart-count");
+    if (cartCountElt)  cartCountElt.textContent = `${cart.length} Items`; 
+}
 
 //------------------- search:
 function togglesearchMode() {
@@ -197,7 +214,8 @@ function showWishlist() {
         if (foundElts) wishlist.push(foundElts[0]);
     }
     fillEltWithArray(wishlist, "itemsByCategory");
-    document.getElementById("category").innerText = "";
+    let categoryElt = document.getElementById("category");
+    if (categoryElt) categoryElt.innerText = "";
 }
 //------------------- checkout:
 async function checkout() {
